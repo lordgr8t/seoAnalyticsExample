eb741b2c41121ded1a7f84119a9146620368f087
lordgr8t <lordgr8t@gmail.com>
2022-09-13 01:01:07 +0300

    tamplate

---
+++ gr8tseo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+	<title>GR8T SEO ANALITICS</title>
+</head>
+<body>
+	
+	<div class="block_pc">
+
+		<div class="link">https://www.tinkoff.ru</div>
+
+		
+
+	</div>
+
+
+
+
+	<style>
+		*{
+			font-family: Roboto;
+		}	
+
+	</style>
+
+</body>
+</html>
