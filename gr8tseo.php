ee69753a53aa3e6adf5bc22ba41b27dc1aeb97d5
lordgr8t <lordgr8t@gmail.com>
2022-09-13 01:26:27 +0300

    google stiles

--- gr8tseo.php
+++ gr8tseo.php
@@ -9,12 +9,50 @@
 <body>
 	
 	<div class="block_pc">
-
 		<div class="link">https://www.tinkoff.ru</div>
-
-		
-
+		<div class="title maintitle">Тинькофф — Кредитные и дебетовые карты</div>
+		<div class="descript">
+			Оформление онлайн и бесплатная доставка: кредитные и дебетовые карты, кредиты, сотовый оператор, страхование и другие услуги для физических и юридических
+		</div>
+		<div class="navIN">
+			<div class="title">Контакты</div>
+			<div class="descropt">Контакты ; Адрес. ул. Хуторская 2-я, д. 38А, стр. 26, Москва ...</div><div class="title">Контакты</div>
+			<div class="descropt">Контакты ; Адрес. ул. Хуторская 2-я, д. 38А, стр. 26, Москва ...</div><div class="title">Контакты</div>
+			<div class="descropt">Контакты ; Адрес. ул. Хуторская 2-я, д. 38А, стр. 26, Москва ...</div><div class="title">Контакты</div>
+			<div class="descropt">Контакты ; Адрес. ул. Хуторская 2-я, д. 38А, стр. 26, Москва ...</div>
+			<div class="more">Другие результаты с сайта tinkoff.ru »
+			</div>
+		</div>
 	</div>
+
+
+
+
+	<div class="block_mp">
+		<div class="link">https://www.tinkoff.ru</div>
+		<div class="title maintitle">Тинькофф — Кредитные и дебетовые карты</div>
+		<div class="descript">
+			Оформление онлайн и бесплатная доставка: кредитные и дебетовые карты, кредиты, сотовый оператор, страхование и другие услуги для физических и юридических
+		</div>
+		<div class="navIN">
+			<hr>
+			<div class="title">Контакты</div>
+			<hr>
+			<div class="title">Контакты</div>
+			<hr>
+			<div class="title">Контакты</div>
+			<hr>
+			<div class="title">Контакты</div>
+			<hr>
+			<div class="title">Контакты</div>
+			<hr>
+			<div class="title">Контакты</div>
+		
+		</div>
+	</div>
+
+
+
 
 
 
@@ -22,9 +60,66 @@
 	<style>
 		*{
 			font-family: Roboto;
-		}	
+		}
+		/*pc*/
+		.block_pc{
+			max-width: 600px;
+			/*border: solid 1px gray;*/
+			margin: 50px auto 0 auto;
+		}
+		.link{
+			color: #202124;
+			font-style: normal;
+			font-size: 14px;
+			line-height: 1.3;
+			cursor: pointer;
+		}
+		.maintitle{
+			margin-bottom: 4px;
+		}
+		.title{
+			font-size: 20px;
+			font-weight: 400;
+			color: #1a0dab;
+		}
+		.title:hover, .more:hover{
+			cursor: pointer;
+			text-decoration: underline;
+		}
+		.descript{
+			color: #4d5156;
+			font-size: 14px;
+		}
+		.block_pc .navIN{
+			margin-top: 20px;
+			margin-left: 25px;
+		}
+		.navIN .descropt{
+			margin-bottom: 20px;
+			color: #4d5156;
+		}
+		.more{
+			color: #1a0dab;
+		}
+
+		/*mp*/
+
+
+		.block_mp{
+			margin: 50px auto 0 auto;
+			max-width: 400px;
+			/*border: solid 1px gray;*/
+		}
+		.block_mp .navIN{
+			margin-top: 10px;
+		}
+		hr{
+			border-top: 1px solid #dadce0;
+			border-bottom: none;
+		}
 
 	</style>
+
 
 </body>
 </html>
